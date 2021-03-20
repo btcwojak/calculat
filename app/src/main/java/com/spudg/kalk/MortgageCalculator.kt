b16faeb3c8133ad4778ac633e12ec14bb0cfb9a5
= <itzsymbaa@outlook.com>
2021-03-20 22:04:58 +0000

    Finished mortgage calculator

--- app/src/main/java/com/spudg/kalk/MortgageCalculator.kt
+++ app/src/main/java/com/spudg/kalk/MortgageCalculator.kt
@@ -2,9 +2,9 @@ package com.spudg.kalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.spudg.kalk.databinding.ActivityMainBinding
 import com.spudg.kalk.databinding.ActivityMortgageCalculatorBinding
+import java.text.DecimalFormat
+import java.text.NumberFormat
 
 class MortgageCalculator : AppCompatActivity() {
 
@@ -16,36 +16,71 @@ class MortgageCalculator : AppCompatActivity() {
         val view = bindingMortCalc.root
         setContentView(view)
 
+        val gbpFormatter: NumberFormat = DecimalFormat("£#,##0.00")
+        val percentFormatter: NumberFormat = DecimalFormat("#,##0.00%")
+
         bindingMortCalc.btnCalculate.setOnClickListener {
 
-            var housePrice = bindingMortCalc.etHousePrice.text.toString().toLong()
-            var deposit = bindingMortCalc.etDeposit.text.toString().toLong()
-            var term = bindingMortCalc.etTerm.text.toString().toLong()
-            var interestRate = bindingMortCalc.etInterest.text.toString().toLong()
+            val housePrice = bindingMortCalc.etHousePrice.text.toString().toFloat()
+            val deposit = bindingMortCalc.etDeposit.text.toString().toFloat()
+            val term = bindingMortCalc.etTerm.text.toString().toFloat()
+            val interestRate = bindingMortCalc.etInterest.text.toString().toFloat()/100
 
-            var borrowing = housePrice - deposit
+            val borrowing = housePrice - deposit
 
-            bindingMortCalc.housePrice.text = "House price - £$housePrice"
-            bindingMortCalc.deposit.text = "Deposit - £$deposit"
-            bindingMortCalc.totalBorrowing.text = "Borrowing - £$borrowing"
-            bindingMortCalc.interestRate.text = "Interest rate - $interestRate%"
-            bindingMortCalc.term.text = "Length - $term years"
+            bindingMortCalc.housePrice.text = gbpFormatter.format(housePrice)
+            bindingMortCalc.deposit.text = gbpFormatter.format(deposit)
+            bindingMortCalc.totalBorrowing.text = gbpFormatter.format(borrowing)
+            bindingMortCalc.interestRate.text = percentFormatter.format(interestRate)
+            bindingMortCalc.term.text = "${term.toInt()} years"
 
-            val annualInterest = borrowing * (interestRate/100F)
-            Log.e("testAnnualInterest",annualInterest.toString())
-            Log.e("testBorrowing",borrowing.toString())
-            Log.e("testInterestRate",(interestRate/100F).toString())
+            var runningTotalInterest = 0F
+            var osBalance = borrowing
 
-            val totalInterest = annualInterest * term
+            while(osBalance > 0) {
+                runningTotalInterest += (osBalance * interestRate)
+                osBalance -= (borrowing / term)
+            }
+
+            val totalInterest = runningTotalInterest
 
             val totalToRepay = borrowing + totalInterest
             val monthlyToRepay = totalToRepay / (term*12)
 
-            bindingMortCalc.totalInterest.text = "Total interest - £$totalInterest"
-            bindingMortCalc.monthlyRepayment.text = "Monthly repayment - £$monthlyToRepay"
+            bindingMortCalc.totalInterest.text = gbpFormatter.format(totalInterest)
+            bindingMortCalc.monthlyRepayment.text = gbpFormatter.format(monthlyToRepay)
 
 
         }
 
     }
+
+    fun currencyInputFilter(string: String, MAX_BEFORE_POINT: Int, MAX_DECIMAL: Int): String {
+        var str = string
+        if (str[0] == '.') str = "0$str"
+        val max = str.length
+        var rFinal = ""
+        var after = false
+        var i = 0
+        var up = 0
+        var decimal = 0
+        var t: Char
+        while (i < max) {
+            t = str[i]
+            if (t != '.' && !after) {
+                up++
+                if (up > MAX_BEFORE_POINT) return rFinal
+            } else if (t == '.') {
+                after = true
+            } else {
+                decimal++
+                if (decimal > MAX_DECIMAL) return rFinal
+            }
+            rFinal += t
+            i++
+        }
+        return rFinal
+    }
+
+
 }
