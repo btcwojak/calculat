68326ae4992bacd49228152c0f18f26ce889d8d3
= <itzsymbaa@outlook.com>
2021-03-21 20:07:02 +0000

    Improved MortgageCalculator.kt and added initial chart

--- app/src/main/java/com/spudg/kalk/MortgageCalculator.kt
+++ app/src/main/java/com/spudg/kalk/MortgageCalculator.kt
@@ -2,9 +2,18 @@ package com.spudg.kalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.content.res.ResourcesCompat
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.spudg.kalk.databinding.ActivityMortgageCalculatorBinding
 import java.text.DecimalFormat
 import java.text.NumberFormat
+import kotlin.math.pow
 
 class MortgageCalculator : AppCompatActivity() {
 
@@ -24,6 +33,7 @@ class MortgageCalculator : AppCompatActivity() {
             val housePrice = bindingMortCalc.etHousePrice.text.toString().toFloat()
             val deposit = bindingMortCalc.etDeposit.text.toString().toFloat()
             val term = bindingMortCalc.etTerm.text.toString().toFloat()
+            val period = 12
             val interestRate = bindingMortCalc.etInterest.text.toString().toFloat()/100
 
             val borrowing = housePrice - deposit
@@ -34,53 +44,86 @@ class MortgageCalculator : AppCompatActivity() {
             bindingMortCalc.interestRate.text = percentFormatter.format(interestRate)
             bindingMortCalc.term.text = "${term.toInt()} years"
 
-            var runningTotalInterest = 0F
-            var osBalance = borrowing
+            val monthlyPayment = (borrowing * (1 + interestRate / period).pow(term * period) * (interestRate / period) / ((1 + interestRate / period).pow(term * period) - 1))
 
-            while(osBalance > 0) {
-                runningTotalInterest += (osBalance * interestRate)
-                osBalance -= (borrowing / term)
+            val monthlyLeftToPay: ArrayList<Float> = arrayListOf()
+            monthlyLeftToPay.add(borrowing)
+            var runningToRepay = borrowing
+            repeat ((term*period).toInt()) {
+                runningToRepay = runningToRepay + (runningToRepay*interestRate)/12 - monthlyPayment
+                monthlyLeftToPay.add(runningToRepay)
             }
 
-            val totalInterest = runningTotalInterest
+            val yearlyLeftToPay: ArrayList<Float> = arrayListOf()
+            (0 until ((period*term)+1).toInt() step 12).asIterable()
+                    .forEach { i ->
+                        yearlyLeftToPay.add(monthlyLeftToPay[i])
+                    }
 
-            val totalToRepay = borrowing + totalInterest
-            val monthlyToRepay = totalToRepay / (term*12)
+            bindingMortCalc.totalInterest.text = gbpFormatter.format((monthlyPayment*term*period)-borrowing)
+            bindingMortCalc.monthlyRepayment.text = gbpFormatter.format(monthlyPayment)
 
-            bindingMortCalc.totalInterest.text = gbpFormatter.format(totalInterest)
-            bindingMortCalc.monthlyRepayment.text = gbpFormatter.format(monthlyToRepay)
-
+            setUpChart(yearlyLeftToPay)
 
         }
 
     }
 
-    fun currencyInputFilter(string: String, MAX_BEFORE_POINT: Int, MAX_DECIMAL: Int): String {
-        var str = string
-        if (str[0] == '.') str = "0$str"
-        val max = str.length
-        var rFinal = ""
-        var after = false
-        var i = 0
-        var up = 0
-        var decimal = 0
-        var t: Char
-        while (i < max) {
-            t = str[i]
-            if (t != '.' && !after) {
-                up++
-                if (up > MAX_BEFORE_POINT) return rFinal
-            } else if (t == '.') {
-                after = true
-            } else {
-                decimal++
-                if (decimal > MAX_DECIMAL) return rFinal
-            }
-            rFinal += t
-            i++
-        }
-        return rFinal
-    }
+    private fun setUpChart(monthlyLeftToPay: ArrayList<Float>) {
 
+        val yearsInTerm: ArrayList<Int> = arrayListOf()
+        repeat (monthlyLeftToPay.size) {
+            yearsInTerm.add(it)
+        }
+
+        val entriesLine: ArrayList<BarEntry> = arrayListOf()
+
+        for (i in 0 until yearsInTerm.size) {
+            entriesLine.add(BarEntry(yearsInTerm[i].toFloat(), monthlyLeftToPay[i]))
+        }
+
+        val dataSetLine = LineDataSet(entriesLine as List<Entry>?, "")
+        val dataLine = LineData(dataSetLine)
+        dataSetLine.color = R.color.colorAccent
+
+        val chartLine: LineChart = bindingMortCalc.leftToPayLineChart
+        if (entriesLine.size > 0) {
+            chartLine.data = dataLine
+        }
+
+
+        chartLine.animateY(800)
+        chartLine.setNoDataText("No data yet.")
+        chartLine.setNoDataTextColor(0xff000000.toInt())
+        chartLine.setNoDataTextTypeface(ResourcesCompat.getFont(this, R.font.open_sans_light))
+        chartLine.xAxis.setDrawGridLines(false)
+        chartLine.axisRight.isEnabled = false
+        chartLine.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        chartLine.legend.isEnabled = false
+
+        dataLine.setValueFormatter(object : ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                return if (value > 0) {
+                    val mFormat = DecimalFormat("###,###,##0.00")
+                    mFormat.format(super.getFormattedValue(value).toFloat())
+                } else {
+                    ""
+                }
+            }
+        })
+
+        chartLine.description.isEnabled = false
+
+        val l: Legend = chartLine.legend
+        l.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+        l.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        l.orientation = Legend.LegendOrientation.HORIZONTAL
+        l.setDrawInside(false)
+
+        chartLine.invalidate()
+
+
+
+    }
 
 }
